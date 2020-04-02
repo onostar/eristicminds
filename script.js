2bb9a181bd0cba470691804119242f6a24acc754
kelly ikpefua <onostarkels@gmail.com>
2020-04-02 23:19:57 +0100

    eristic minds is a political blog
    Eristic minds is a website that serve up political events and blog

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayMenu(){
+	var myMenu = document.getElementById("menu-icon");
+	var xpand = "<ul><li><a href='index.html'>Home</a></li><li><a href='news.html' title='latest Trends'>News</a></li><li><a href='events.html' title='Happenings'>Events</a></li><li><a href='gossip.html' title='Aproko news'>Gossip</a></li><li><a href='archive.html' title='Posts'>Archive</a></li></ul>";
+	myMenu.innerHTML = xpand;
+	}
