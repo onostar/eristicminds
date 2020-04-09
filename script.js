7209f47e1c6adaedb57f9a2a0ba20b8659977ff0
kelly ikpefua <onostarkels@gmail.com>
2020-04-09 20:36:29 +0100

    update website

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function displayMenu(){
-	var myMenu = document.getElementById("menu-icon");
-	var xpand = "<ul><li><a href='index.html'>Home</a></li><li><a href='news.html' title='latest Trends'>News</a></li><li><a href='events.html' title='Happenings'>Events</a></li><li><a href='gossip.html' title='Aproko news'>Gossip</a></li><li><a href='archive.html' title='Posts'>Archive</a></li></ul>";
-	myMenu.innerHTML = xpand;
+	let menu = document.getElementById('navigation');
+	if(menu.style.display == 'block'){
+		menu.style.display = 'none';
 	}
+	else{
+		menu.style.display = 'block';
+	}
+}
